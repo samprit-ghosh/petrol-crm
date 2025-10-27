2013a5fcec527139330d60e605f0b60bb92cece2
samprit-ghosh <sampritghosh18@gmail.com>
2025-10-27 15:25:07 +0530

    solve render

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api || ',
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'https://petrol-crm.onrender.com/api',
 });
+
 // Add token to requests
 axiosInstance.interceptors.request.use(
   (config) => {
